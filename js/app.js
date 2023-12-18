112d1c461cfab80f58423e90b0168d36d53bcfdd
Seong il Lee <118361080+Sun6004@users.noreply.github.com>
2023-12-18 17:12:50 +0900

    임시commit

--- js/app.js
+++ js/app.js
@@ -1,24 +1,82 @@
 const logInBtn = document.querySelector("#logInBtn");
 const userName = document.querySelector("#inp");
 const res = document.querySelector("#res");
+const loginForm = document.querySelector("#logIn_form");
+const logOutBtn = document.querySelector("#logOutBtn");
+const clockVal = document.querySelector("#clock");
 
-console.log(userName);
-
-function logIn(){
-    const val = userName.value;
-    if (val === ""){
-        alert("plz write your name");
-    }else if(val.length > 10){
-        alert("too Long");
-    }else{
-        console.log(val);
+function loginCheck() {
+    const userCheck = localStorage.getItem("name");
+    if (userCheck === null) {
+        loginForm.classList.remove("hidden");
+        res.classList.add("hidden");
+    } else {
+        loginForm.classList.add("hidden");
+        res.classList.remove("hidden");
     }
-    console.log(val);
 }
-function loginSuccess(){
-    const val = userName.value;
-    res.innerHTML(val);
+
+loginCheck();
+
+function loginSuccess(val) {
+    loginForm.classList.add("hidden");
+    res.innerHTML = `<h3>hi ${val}</h3>
+                    <button type="submit" id="logOutBtn">Log Out</button>`;
     res.classList.remove('hidden');
-}   
+}
+
+function logIn(event) {
+    event.preventDefault();
+    const val = userName.value;
+    if (val === "") {
+        alert("Please write your name");
+    } else if (val.length > 10) {
+        alert("Too long");
+    } else {
+        console.log(val);
+        localStorage.setItem("name", val);
+        console.log(val);
+        loginSuccess(val);
+    }
+}
+
+function log_out(event){
+    event.preventDefault();
+    loginForm.classList.remove("hidden");
+    res.classList.add('hidden');
+    localStorage.clear();
+}
+
+class ClockC {
+    constructor({ template }) {
+        this.template = template;
+        this.timer = null;
+    }
+  
+    render() {
+        let date = new Date();
+        let hours = date.getHours();
+        let mins = date.getMinutes();
+        let secs = date.getSeconds();
+  
+        if (hours < 10) hours = "0" + hours;
+        if (mins < 10) mins = "0" + mins;
+        if (secs < 10) secs = "0" + secs;
+  
+        let output = this.template.replace('h', hours).replace('m', mins).replace('s', secs);
+  
+        console.log(output);
+        clockVal.innerText = `${output}`;
+    }
+}
+
+const myClock = new ClockC({ template: 'h:m:s' });
+myClock.render();
 
 logInBtn.addEventListener("click", logIn);
+document.addEventListener('click', function (event) {
+    if (event.target.id === 'logOutBtn') {
+        log_out(event);
+    }
+});
+
